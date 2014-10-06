f4ce53e87b6538559bc9af0c9d6803d75df7a482
Allan Sandfeld Jensen <allan.jensen@digia.com>
2014-10-06 17:07:26 +0200

    Fix assertion in PerformanceTiming
    We get negative values in monotonic increasing time because we forget
    to initialize the start time.
    
    Change-Id: I292792f8f996025e7f0254bb90391b7af6873877
    Reviewed-by: Michael Bruning <michael.bruning@digia.com>

--- Source/WTF/wtf/CurrentTime.cpp
+++ Source/WTF/wtf/CurrentTime.cpp
@@ -270,6 +270,8 @@ double monotonicallyIncreasingTime()
 {
     ASSERT(QElapsedTimer::isMonotonic());
     static QElapsedTimer timer;
+    if (!timer.isValid())
+        timer.start();
     return timer.nsecsElapsed() / 1.0e9;
 }
 
