aaf7278d725bfca56d6f013a043565ded31a6169
Konstantin Tokarev <annulen@yandex.ru>
2016-01-09 22:00:14 +0300

    fix wip
    Change-Id: I99ac357db051ffd408f1f7983c8f4f1f7da3147f

--- Source/JavaScriptCore/assembler/MacroAssemblerMIPS.h
+++ Source/JavaScriptCore/assembler/MacroAssemblerMIPS.h
@@ -637,11 +637,6 @@ public:
         m_assembler.xorInsn(dest, src, immTempRegister);
     }
 
-    void countLeadingZeros32(RegisterID, RegisterID)
-    {
-        RELEASE_ASSERT_NOT_REACHED();
-    }
-
     void sqrtDouble(FPRegisterID src, FPRegisterID dst)
     {
         m_assembler.sqrtd(dst, src);
