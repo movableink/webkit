ybandou/WPEWebKit

MIPS: use pufirfyNaN for substraction result in slow_op_sub
On MIPS, glibc's slowfp implementation of double substraction yields an
impure NaN.

Source/JavaScriptCore/runtime/CommonSlowPaths.cpp
@@ -386,7 +386,14 @@ SLOW_PATH_DECL(slow_path_sub)
     BEGIN();
     double a = OP_C(2).jsValue().toNumber(exec);
     double b = OP_C(3).jsValue().toNumber(exec);
+
+#if CPU(MIPS) && defined(__GLIBC__)
+    // on MIPS, the GLIBC version of the softfp double substraction sometimes
+    // returns impure NaNs
+    RETURN(jsNumber(purifyNaN(a - b)));
+#else
     RETURN(jsNumber(a - b));
+#endif
 }
 
 SLOW_PATH_DECL(slow_path_div)

movableink/webkit

Change-Id: I06bc54dee172802709f02e1f7755ae1db3049863

Source/JavaScriptCore/runtime/CommonSlowPaths.cpp
@@ -390,7 +390,14 @@ SLOW_PATH_DECL(slow_path_sub)
     BEGIN();
     double a = OP_C(2).jsValue().toNumber(exec);
     double b = OP_C(3).jsValue().toNumber(exec);
+
+#if CPU(MIPS) && defined(__GLIBC__)
+    // on MIPS, the GLIBC version of the softfp double substraction sometimes
+    // returns impure NaNs
+    RETURN_WITH_RESULT_PROFILING(jsNumber(purifyNaN(a - b)));
+#else
     RETURN_WITH_RESULT_PROFILING(jsNumber(a - b));
+#endif
 }
 
 SLOW_PATH_DECL(slow_path_div)