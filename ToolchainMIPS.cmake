e771591eeb79f68de66522b2594178052ff8ff60
Konstantin Tokarev <annulen@yandex.ru>
2016-03-24 19:33:48 +0300

    fixup! Added toolcahin file for MIPS
    Change-Id: I66ae05651108b4e45918081fedf419608f5f332b

--- ToolchainMIPS.cmake
+++ ToolchainMIPS.cmake
@@ -1,29 +1,29 @@
-INCLUDE(CMakeForceCompiler)
+include(CMakeForceCompiler)
 
 # this one is important
-SET(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_NAME Linux)
 #this one not so much
-SET(CMAKE_SYSTEM_VERSION 1)
+set(CMAKE_SYSTEM_VERSION 1)
 
-SET(CMAKE_SYSTEM_PROCESSOR mips)
+set(CMAKE_SYSTEM_PROCESSOR mips)
 
 #
 # stbgcc toolchain
 #
-SET(STBGCC_PATH /opt/toolchains/stbgcc-4.8-1.2)
+set(STBGCC_PATH /opt/toolchains/stbgcc-4.8-1.2)
 
 #
 # specify the cross compiler
 #
-SET(CMAKE_C_COMPILER   ${STBGCC_PATH}/bin-ccache/mipsel-linux-gcc)
-SET(CMAKE_CXX_COMPILER ${STBGCC_PATH}/bin-ccache/mipsel-linux-g++)
+set(CMAKE_C_COMPILER   ${STBGCC_PATH}/bin-ccache/mipsel-linux-gcc)
+set(CMAKE_CXX_COMPILER ${STBGCC_PATH}/bin-ccache/mipsel-linux-g++)
 
 # where is the target environment
-SET(CMAKE_FIND_ROOT_PATH ${STBGCC_PATH}/target)
+set(CMAKE_FIND_ROOT_PATH ${STBGCC_PATH}/target)
 
 # search for programs in the build host directories
-SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 # for libraries and headers in the target directories
-SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
