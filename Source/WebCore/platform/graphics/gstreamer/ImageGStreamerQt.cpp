6530f479a61dbc592a618a77393c3da3dd1ba8ee
Michael Nutt <michael@nuttnet.net>
2023-06-28 13:59:56 -0400

    ImageStreamer uses RefPtr now

--- Source/WebCore/platform/graphics/gstreamer/ImageGStreamerQt.cpp
+++ Source/WebCore/platform/graphics/gstreamer/ImageGStreamerQt.cpp
@@ -30,9 +30,9 @@
 
 namespace WebCore {
 
-ImageGStreamer::ImageGStreamer(GstSample* sample)
+ImageGStreamer::ImageGStreamer(GRefPtr<GstSample>&& sample)
 {
-    GstCaps* caps = gst_sample_get_caps(sample);
+    GstCaps* caps = gst_sample_get_caps(sample.get());
     GstVideoInfo videoInfo;
     gst_video_info_init(&videoInfo);
     if (!gst_video_info_from_caps(&videoInfo, caps))
@@ -42,7 +42,7 @@ ImageGStreamer::ImageGStreamer(GstSample* sample)
     // Right now the TextureMapper only supports chromas with one plane
     ASSERT(GST_VIDEO_INFO_N_PLANES(&videoInfo) == 1);
 
-    GstBuffer* buffer = gst_sample_get_buffer(sample);
+    GstBuffer* buffer = gst_sample_get_buffer(sample.get());
     if (UNLIKELY(!GST_IS_BUFFER(buffer)))
         return;
 
