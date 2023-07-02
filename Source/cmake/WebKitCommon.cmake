1468f8ca10343ccb77de278f2b03f7d4bccf38eb
Michael Nutt <michael@nuttnet.net>
2023-07-02 13:46:08 -0400

    update build scripts

--- Source/cmake/WebKitCommon.cmake
+++ Source/cmake/WebKitCommon.cmake
@@ -47,7 +47,7 @@ if (NOT HAS_RUN_WEBKIT_COMMON)
         WPE
         WinCairo
     )
-    set(PORT "NOPORT" CACHE STRING "choose which WebKit port to build (one of ${ALL_PORTS})")
+    set(PORT "Qt" CACHE STRING "choose which WebKit port to build (one of ${ALL_PORTS})")
 
     list(FIND ALL_PORTS ${PORT} RET)
     if (${RET} EQUAL -1)
