19dbe62c0ea00782845043556eb7385cfdc99ab1
Michael Nutt <michael@nuttnet.net>
2023-07-14 20:53:16 -0400

    ensure explicit String(Vector<UChar, 64>)

--- Source/WTF/wtf/text/WTFString.h
+++ Source/WTF/wtf/text/WTFString.h
@@ -266,10 +266,11 @@ public:
 #if PLATFORM(QT)
     WTF_EXPORT_PRIVATE String(const QString&);
     WTF_EXPORT_PRIVATE String(const QLatin1StringView&);
-    WTF_EXPORT_PRIVATE explicit String(const QStringView&);
+    WTF_EXPORT_PRIVATE String(const QStringView&);
     WTF_EXPORT_PRIVATE operator QString() const;
 
     // String(QStringView) makes for an ambiguous constructor, so we need to make these explicit
+    ALWAYS_INLINE String(Vector<UChar, 64> characters) : String(characters.data(), characters.size()) {}
     ALWAYS_INLINE String(Vector<UChar, 32> characters) : String(characters.data(), characters.size()) {}
     ALWAYS_INLINE String(Vector<UChar> characters) : String(characters.data(), characters.size()) {}
 #endif
