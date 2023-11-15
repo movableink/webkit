3e331782c0f2ffae935ca083860719f6e72f9f2b
Michael Nutt <michael@nuttnet.net>
2023-11-15 16:49:05 -0500

    better checking for emoji fonts

--- Source/WebCore/platform/graphics/qt/FontCascadeQt.cpp
+++ Source/WebCore/platform/graphics/qt/FontCascadeQt.cpp
@@ -332,6 +332,22 @@ void FontCascade::drawGlyphs(GraphicsContext& context, const Font& font, const G
     drawQtGlyphRun(context, qtGlyphs, point, /* baselineOffset = */0);
 }
 
+ResolvedEmojiPolicy FontCascade::resolveEmojiPolicy(FontVariantEmoji fontVariantEmoji, UChar32)
+{
+    // FIXME: https://bugs.webkit.org/show_bug.cgi?id=259205 We can't return RequireText or RequireEmoji
+    // unless we have a way of knowing whether a font/glyph is color or not.
+    switch (fontVariantEmoji) {
+    case FontVariantEmoji::Normal:
+    case FontVariantEmoji::Unicode:
+        return ResolvedEmojiPolicy::NoPreference;
+    case FontVariantEmoji::Text:
+        return ResolvedEmojiPolicy::RequireText;
+    case FontVariantEmoji::Emoji:
+        return ResolvedEmojiPolicy::RequireEmoji;
+    }
+    return ResolvedEmojiPolicy::NoPreference;
+}
+
 
 //bool FontCascade::canExpandAroundIdeographsInComplexText()
 //{
