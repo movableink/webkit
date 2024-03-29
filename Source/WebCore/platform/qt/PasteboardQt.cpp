04835b32062591721a0124a0300a417a4121104b
Michael Nutt <michael@nuttnet.net>
2024-03-29 07:51:09 -0400

    fix initialization order

--- Source/WebCore/platform/qt/PasteboardQt.cpp
+++ Source/WebCore/platform/qt/PasteboardQt.cpp
@@ -101,8 +101,8 @@ std::unique_ptr<Pasteboard> Pasteboard::create(const DragData& dragData)
 #endif
 
 Pasteboard::Pasteboard(std::unique_ptr<PasteboardContext>&& context, const QMimeData* readableClipboard, bool isForDragAndDrop)
-    : m_selectionMode(false)
-    , m_context(WTFMove(context))
+    : m_context(WTFMove(context))
+    , m_selectionMode(false)
     , m_readableData(readableClipboard)
     , m_writableData(0)
     , m_isForDragAndDrop(isForDragAndDrop)
@@ -110,8 +110,8 @@ Pasteboard::Pasteboard(std::unique_ptr<PasteboardContext>&& context, const QMime
 }
 
 Pasteboard::Pasteboard(std::unique_ptr<PasteboardContext>&& context)
-    : m_selectionMode(false)
-    , m_context(WTFMove(context))
+    : m_context(WTFMove(context))
+    , m_selectionMode(false)
     , m_readableData(0)
     , m_writableData(0)
 {
